30845ae490d760bc38b78d0f755d544c809010ad
pennuel <pjmakiarealestate@gmail.com>
2022-03-10 09:22:54 +0000

    Added s3 bucket module

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "address_space" {
