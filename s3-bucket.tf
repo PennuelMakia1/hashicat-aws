1924494d97bac899e8c1ac149f5b7f4d1b3b2009
pennuel <pjmakiarealestate@gmail.com>
2022-03-10 09:08:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/pennuel123/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = var.prefix
-  acceleration_status = enabled
+  acceleration_status = "enabled"
   bucket = "pdf"
 }
